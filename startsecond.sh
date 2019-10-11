3aca5a2d881aec2a4b38f496e48c7389adf80e16
Skyboxpro <46995474+Skyboxpro@users.noreply.github.com>
2019-10-12 10:04:19 +1100

    Update startsecond.sh

--- startsecond.sh
+++ startsecond.sh
@@ -5,5 +5,5 @@
 sleep 30
 
 cd /home/pi/go/bin 
-nohup ./skywire-node -connect-manager -manager-address "$MANIP":5998 -manager-web "$MANIP":8000 -discovery-address testnet.skywire.skycoin.com:5999-028ec969bdeb92a1991bb19c948645ac8150468a6919113061899051409de3f243 -address :5000 -web-port :6001 > /dev/null 2>&1 &
+nohup ./skywire-node -connect-manager -manager-address 192.168.81.101:5998 -manager-web 192.168.81.101:8000 -discovery-address testnet.skywire.skycoin.com:5999-028ec969bdeb92a1991bb19c948645ac8150468a6919113061899051409de3f243 -address :5000 -web-port :6001 > /dev/null 2>&1 &
 
